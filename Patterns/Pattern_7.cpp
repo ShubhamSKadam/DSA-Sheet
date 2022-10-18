4562ff2224c94bb0c0d811f767e47d55338e7f6c
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-10-18 12:44:01 +0530

    Pattern_7 Added

--- Patterns/Pattern_7.cpp
+++ Patterns/Pattern_7.cpp
@@ -1,11 +1,16 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main() {
 
 	int n;
 	cin >> n;
 
-
-
+	for (int i = 1; i <= 2 * n - 1; i++) {
+		int col = i > n ? 2 * n - i : i;
+		for (int j = 1; j <= col; j++) {
+			cout << "*" << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }
