2c1a294e526d40dc4a873f7afee5812ab0b0f2c7
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-10-18 16:55:14 +0530

    Diamond Pattern

--- Patterns/Pattern_11.cpp
+++ Patterns/Pattern_11.cpp
@@ -5,13 +5,17 @@ int main() {
 	int n;
 	cin >> n;
 
-	for (int i = 1; i <= 2 * n - 2; i++) {
+	for (int i = 1; i <= 2 * n; i++) {
 
-		int space = i > n ? i - n : n - i - 1;
-		for (int k = 1; k <= space ; k++) {
+		int space = i <= n ? n - i : i - n - 1;
+		for (int k = 1; k <= space ; k++)
 			cout << " ";
-		}
-	}
 
+		int col = i <= n ? i : 2 * n - i + 1;
+		for (int j = 1; j <= col; j++)
+			cout << "*" << " ";
+
+		cout << endl;
+	}
 	return 0;
 }
