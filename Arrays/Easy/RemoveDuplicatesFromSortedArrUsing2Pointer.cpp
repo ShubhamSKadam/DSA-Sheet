a1ea40b7b0b60c4962f276aaef7e70a421269a3d
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-12-05 19:49:02 +0530

    deleted executables

--- Arrays/Easy/RemoveDuplicatesFromSortedArrUsing2Pointer.cpp
+++ Arrays/Easy/RemoveDuplicatesFromSortedArrUsing2Pointer.cpp
@@ -1,39 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-int removeDuplicates(int *arr, int n) {
-
-	int i = 0, j = 1;
-	while (j < n || i > j) {
-
-		if (arr[i] == arr[j] || i == j)
-			j++;
-
-		else {
+int removeDuplicates(int *arr,int n){
+	int i=0;
+	
+	for (int j = 1; j < n; j++)
+	{
+		if(arr[i]!=arr[j]){
 			i++;
-			if (i != j)
-				arr[i] = arr[j];
+			arr[i] = arr[j];
 		}
 	}
-	return i + 1;
+	return i+1;	
 }
-int main() {
+int main(){
 
 	int n;
-	cin >> n;
+	cin>>n;
 
 	int *arr = new int[n];
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i];
+		cin>>arr[i];
 	}
 
-	int k = removeDuplicates(arr, n);
-	for (int i = 0; i < k; ++i)
+	int k = removeDuplicates(arr,n);
+	for (int i = 0; i < k; i++)
 	{
-		cout << arr[i] << " ";
+		cout<<arr[i]<<" ";
 	}
-	cout << endl;
-	delete [] arr;
-
-	return 0;
+	cout<<endl;
+	
+	delete [] arr;	
 }
