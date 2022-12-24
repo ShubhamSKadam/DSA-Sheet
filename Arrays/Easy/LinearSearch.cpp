09b9b5ce812f8ea2ea09e12ed767ce363de8a229
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-12-24 17:23:29 +0530

    Linear Search Program

---
+++ Arrays/Easy/LinearSearch.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+
+	int *arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	int x;
+	cin >> x;
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (arr[i] == x) {
+			cout << "Element found at index " << i << endl;
+			return 0;
+		}
+	}
+	cout << "Element not found" << endl;
+	return -1;
+	delete [] arr;
+}
